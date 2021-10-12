271359685b633a87a4466cef9199408149058e28
GavryshAlex <alexgavryshua@gmail.com>
2021-10-12 21:07:27 +0300

    Initial commit

---
+++ ImageTest/MainWindow.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ImageTest
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.OpenFille = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.CloseBtn = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// OpenFille
+			// 
+			this.OpenFille.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.OpenFille.Location = new System.Drawing.Point(11, 7);
+			this.OpenFille.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.OpenFille.Name = "OpenFille";
+			this.OpenFille.Size = new System.Drawing.Size(165, 36);
+			this.OpenFille.TabIndex = 0;
+			this.OpenFille.Text = "Open File From PC";
+			this.OpenFille.UseVisualStyleBackColor = false;
+			this.OpenFille.Click += new System.EventHandler(this.OpenFille_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.pictureBox1.Location = new System.Drawing.Point(11, 50);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(343, 288);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox2.Location = new System.Drawing.Point(369, 50);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(332, 288);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// CloseBtn
+			// 
+			this.CloseBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.CloseBtn.ForeColor = System.Drawing.Color.White;
+			this.CloseBtn.Location = new System.Drawing.Point(535, 7);
+			this.CloseBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.CloseBtn.Name = "CloseBtn";
+			this.CloseBtn.Size = new System.Drawing.Size(165, 36);
+			this.CloseBtn.TabIndex = 3;
+			this.CloseBtn.Text = "Close Application";
+			this.CloseBtn.UseVisualStyleBackColor = false;
+			this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.ItemHeight = 16;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Інвертування",
+            "Зміна на постійне значення",
+            "Розбивка: Red",
+            "Розбивка: Green",
+            "Розбивка: Blue",
+            "Розмиття x2"});
+			this.comboBox1.Location = new System.Drawing.Point(345, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(184, 24);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(711, 347);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.CloseBtn);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.OpenFille);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "MainWindow";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenFille;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button CloseBtn;
+		private System.Windows.Forms.ComboBox comboBox1;
+	}
+}
+
---
+++ ImageTest/MainWindow.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Math;
+namespace ImageTest
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OpenFille_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "jpg files (*.jpg)|*.jpg";
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog1.FileName;
+
+                    Bitmap fileImage = new Bitmap(filePath);
+                    pictureBox1.Image = fileImage;
+                    CreateNewImage(fileImage);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error Reading of JPG-File!");
+            }
+        }
+        private void CreateNewImage(Bitmap InputImage)
+        {
+            switch (comboBox1.SelectedIndex)
+			{
+                case 0: // invertion
+                    CreateNewImageInvertion(InputImage);
+                    break;
+                case 1: // change on const value
+                    CreateNewImageChangeOnConstValue(InputImage, 100, 0, 0);
+                    break;
+                case 2: // red
+                    CreateNewImageRed(InputImage);
+                    break;
+                case 3: // green
+                    CreateNewImageGreen(InputImage);
+                    break;
+                case 4: // blue
+                    CreateNewImageBlue(InputImage);
+                    break;
+
+            }
+            
+        }
+        private void CreateNewImageInvertion(Bitmap InputImage)
+        {
+            int m = InputImage.Width, n = InputImage.Height;
+            int i, j;
+            Bitmap OutputImage = new Bitmap(m, n);
+
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                {
+                    Color color_of_Pixel = InputImage.GetPixel(i, j);
+                    Color New_color_of_Pixel = Color.FromArgb(255-color_of_Pixel.R,
+                                                                255 - color_of_Pixel.G,
+                                                                255 - color_of_Pixel.B);
+                    OutputImage.SetPixel(i, j, New_color_of_Pixel);
+                }
+            pictureBox2.Image = OutputImage;
+        }
+        private void CreateNewImageChangeOnConstValue(Bitmap InputImage, int valueR, int valueG, int valueB)
+        {
+            int m = InputImage.Width, n = InputImage.Height;
+            int i, j;
+            Bitmap OutputImage = new Bitmap(m, n);
+
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                {
+                    Color color_of_Pixel = InputImage.GetPixel(i, j);
+                    Color New_color_of_Pixel = Color.FromArgb(Max(Min(color_of_Pixel.R + valueR,255),0),
+                                                               Max(Min(color_of_Pixel.G + valueG,255),0),
+                                                                Max(Min(color_of_Pixel.B + valueB, 255), 0));
+                    OutputImage.SetPixel(i, j, New_color_of_Pixel);
+                }
+            pictureBox2.Image = OutputImage;
+        }
+        private void CreateNewImageRed(Bitmap InputImage)
+        {
+            int m = InputImage.Width, n = InputImage.Height;
+            int i, j;
+            Bitmap OutputImage = new Bitmap(m, n);
+
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                {
+                    Color color_of_Pixel = InputImage.GetPixel(i, j);
+                    Color New_color_of_Pixel = Color.FromArgb(color_of_Pixel.R,
+                                                                0,
+                                                                0);
+                    OutputImage.SetPixel(i, j, New_color_of_Pixel);
+                }
+            pictureBox2.Image = OutputImage;
+        }
+        private void CreateNewImageGreen(Bitmap InputImage)
+        {
+            int m = InputImage.Width, n = InputImage.Height;
+            int i, j;
+            Bitmap OutputImage = new Bitmap(m, n);
+
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                {
+                    Color color_of_Pixel = InputImage.GetPixel(i, j);
+                    Color New_color_of_Pixel = Color.FromArgb(0,
+                                                                color_of_Pixel.G,
+                                                                0);
+                    OutputImage.SetPixel(i, j, New_color_of_Pixel);
+                }
+            pictureBox2.Image = OutputImage;
+        }
+        private void CreateNewImageBlue(Bitmap InputImage)
+        {
+            int m = InputImage.Width, n = InputImage.Height;
+            int i, j;
+            Bitmap OutputImage = new Bitmap(m, n);
+
+            for (i = 0; i < m; i++)
+                for (j = 0; j < n; j++)
+                {
+                    Color color_of_Pixel = InputImage.GetPixel(i, j);
+                    Color New_color_of_Pixel = Color.FromArgb(0,
+                                                                0,
+                                                                color_of_Pixel.B);
+                    OutputImage.SetPixel(i, j, New_color_of_Pixel);
+                }
+            pictureBox2.Image = OutputImage;
+        }
+    }
+}
